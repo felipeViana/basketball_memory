f1f47d767390e9502cd6ef834e42bd5ba9e95f2e
Felipe Viana <felipe.140492@gmail.com>
2020-08-23 18:46:37 -0300

    fix goBack button

--- src/menu/stageSelection.lua
+++ src/menu/stageSelection.lua
@@ -1,12 +1,12 @@
 local sceneManager = require 'src/common/sceneManager'
-local buttonManager = (require 'src/components/buttonManager').new()
+local buttonManager = require 'src/components/buttonManager'
 local dictionary = require 'src/common/dictionary'
 local assets = require 'src/common/assets'
 
 local menu = {};
 local music;
 
-local verticalScroll = 0;
+local verticalScroll;
 local scrollAmount = 200;
 
 local function goToStage11()
@@ -74,120 +74,127 @@ local function goBack()
 end
 
 function menu.load()
-  buttonManager:load()
-  buttonManager:newTextButton({
+  verticalScroll = 0;
+
+  scrollableButtonManager = buttonManager.new()
+  scrollableButtonManager:load()
+
+  fixedButtonManager = buttonManager.new()
+  fixedButtonManager:load()
+
+  scrollableButtonManager:newTextButton({
     text = 'Stage 1-1',
     fn = goToStage11,
     x = 100,
     y = 50,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 1-2',
     fn = goToStage12,
     x = 100,
     y = 150,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 1-3',
     fn = goToStage13,
     x = 100,
     y = 250,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 1-4',
     fn = goToStage14,
     x = 100,
     y = 350,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 1-5',
     fn = goToStage15,
     x = 100,
     y = 450,
   })
 
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 2-1',
     fn = goToStage21,
     x = 450,
     y = 50,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 2-2',
     fn = goToStage22,
     x = 450,
     y = 150,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 2-3',
     fn = goToStage23,
     x = 450,
     y = 250,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 2-4',
     fn = goToStage24,
     x = 450,
     y = 350,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 2-5',
     fn = goToStage25,
     x = 450,
     y = 450,
   })
 
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-1',
     fn = goToStage31,
     x = 800,
     y = 50,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-2',
     fn = goToStage32,
     x = 800,
     y = 150,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-3',
     fn = goToStage33,
     x = 800,
     y = 250,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-4',
     fn = goToStage34,
     x = 800,
     y = 350,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-5',
     fn = goToStage35,
     x = 800,
     y = 450,
   })
 
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-6',
     fn = goToStage35,
     x = 800,
     y = 550,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-7',
     fn = goToStage35,
     x = 800,
     y = 650,
   })
-  buttonManager:newTextButton({
+  scrollableButtonManager:newTextButton({
     text = 'Stage 3-8',
     fn = goToStage35,
     x = 800,
     y = 750,
   })
 
-  buttonManager:newTextButton({
+  fixedButtonManager:newTextButton({
     text = dictionary.localize('GoBack'),
     fn = goBack,
     x = 100,
@@ -199,31 +206,34 @@ function menu.load()
 end
 
 function menu.unload()
-  buttonManager:unload()
+  scrollableButtonManager:unload()
+  fixedButtonManager:unload()
   music:pause()
 end
 
 function menu.update(dt)
-  buttonManager:update(dt, verticalScroll)
+  scrollableButtonManager:update(dt, verticalScroll)
+  fixedButtonManager:update(dt)
 end
 
 function menu.draw()
   love.graphics.push()
   love.graphics.translate(0, verticalScroll)
-  buttonManager:draw()
+  scrollableButtonManager:draw()
   love.graphics.pop()
+
+  fixedButtonManager:draw()
 end
 
 function menu.mousereleased(_, _, button)
-  buttonManager:mouseReleased(button)
+  scrollableButtonManager:mouseReleased(button)
+  fixedButtonManager:mouseReleased(button)
 end
 
 function menu.keypressed(key)
   if key == 'up' then
     verticalScroll = verticalScroll + scrollAmount;
-  end
-
-  if key == 'down' then
+  elseif key == 'down' then
     verticalScroll = verticalScroll - scrollAmount;
   end
 end
